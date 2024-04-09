8e909efa51b6e5a453bdec6ec5508ee12944069a
romicons <romina.constantinoff@gmail.com>
2024-04-09 18:36:40 -0300

    Fixed an issue with the display of the h3 in the nav.

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-//    ACTIVATE DARK/LIGHT MODE
 
-let darkMode = localStorage.getItem('darkMode') === 'true';
-const darkModeToggle = document.getElementById('btn-darkmode');
-
-const toggleDarkMode = () => {
-    darkMode = !darkMode;
-    if (darkMode) {
-        document.body.classList.add('darkmode');
-        document.getElementById('darkmode-btn-description').innerHTML = `Light Mode`
-    } else {
-        document.body.classList.remove('darkmode');
-        document.getElementById('darkmode-btn-description').innerHTML = `Dark Mode`
-    }
-    localStorage.setItem('darkMode', darkMode);
-}
+//      RESPONSIVE DESIGN
 
 darkModeToggle.addEventListener('click', toggleDarkMode);
+
+document.getElementById('hamb-menu').addEventListener('click', toggleMobileNav)
